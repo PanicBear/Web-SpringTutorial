bed8706eb128c7b93e2d809c1930ea1393850137
ClarXo <imp_rove@naver.com>
2020-09-02 15:19:38 +0900

    200902 Spring - Reservation Proeject Replace DB Data with application.properties

--- reservation/src/main/java/kr/co/netmania/reservation/config/DBConfig.java
+++ reservation/src/main/java/kr/co/netmania/reservation/config/DBConfig.java
@@ -3,30 +3,32 @@ package kr.co.netmania.reservation.config;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
+@PropertySource("classpath:application.properties")
 @Configuration
 @EnableTransactionManagement
 public class DBConfig implements TransactionManagementConfigurer {
 
-	// application.properties에서 값 받아오려고 했는데 안됨
-	
-	//@Value("${spring.datasource.driverClassName}")
-	private String driverClassName = "com.mysql.cj.jdbc.Driver";
-	//@Value("${spring.datasource.url}")
-	private String url = "jdbc:mysql://localhost:3306/connectdb?serverTimezone=Asia/Seoul&useUnicode=true&characterEncoding=utf8";
-	//@Value("${spring.datasource.username}")
-	private String username = "connectuser";
-	//@Value("${spring.datasource.password}")
-	private String password = "connect123!@#";
+	// spring project가 아니라 maven webapp으로 만들었기에
+	// application.properties의 위치를 몰라 정해줘야함
+	// 상단의 ProperySource(~)
+	@Value("${spring.datasource.driverClassName}")
+	private String driverClassName;
+	@Value("${spring.datasource.url}")
+	private String url;
+	@Value("${spring.datasource.username}")
+	private String username;
+	@Value("${spring.datasource.password}")
+	private String password;
 
 	@Bean
 	public DataSource dataSource() {
